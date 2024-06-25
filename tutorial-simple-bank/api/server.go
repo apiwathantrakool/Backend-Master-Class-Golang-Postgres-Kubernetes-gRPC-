23f6df3eb5ed840b68c25956fbd917d961d8bbf5
apiwathantrakool <nekky.apiwat@gmail.com>
2024-06-25 17:44:45 +0700

    server: add token

--- tutorial-simple-bank/api/server.go
+++ tutorial-simple-bank/api/server.go
@@ -1,19 +1,30 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/tutorial/simple-bank/db/sqlc"
+	"github.com/tutorial/simple-bank/token"
+	"github.com/tutorial/simple-bank/util"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{config: config, store: store, tokenMaker: tokenMaker}
 	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
@@ -27,7 +38,7 @@ func NewServer(store db.Store) *Server {
 	router.POST("/users", server.createUser)
 
 	server.router = router
-	return server
+	return server, nil
 }
 
 func (server *Server) Start(address string) error {
